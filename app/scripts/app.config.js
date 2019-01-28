090da58ac4ec4fff3a340a686d538654e3d5ae88
Joshua Barnett <joecoolatbyu@ldschurch.org>
2019-01-28 15:53:12 -0700

    trying to get css to work

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,14 +1,16 @@
-angular.module('psJwtApp').config(function ($urlRouterProvider, $stateProvider){
-       $urlRouterProvider.otherwise('/');
+'use strict';
 
-       $stateProvider
-       .state('main', {
-       url: '/',
-       templateUrl: '/views/main.html'
-       })
+angular.module('psJwtApp').config(function ($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise('/');
 
-       .state('register', {
-       url: '/register',
-       templateUrl: '/views/register.html'
-       });
-  })
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: '/views/main.html'
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: '/views/register.html'
+    });
+});
